b0fbe7dbfd3bd86fee5dc8b9df61efa2f12d3700
USER <USER@USER-PC>
2017-10-29 00:42:55 +0530

    first commit

---
+++ DirectNetwork/src/main/java/com/direct/network/DirectNetworkApplication.java
@@ -0,0 +1,12 @@
+package com.direct.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectNetworkApplication.class, args);
+	}
+}
